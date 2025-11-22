04a55e496e75ee6baeb90a9475fb19d5b0d8e478
yogaraj-1 <yogarajp23it@psnacet.edu.in>
2025-11-22 19:50:16 +0530

    incresing size

--- backend/src/index.js
+++ backend/src/index.js
@@ -16,7 +16,7 @@ dotenv.config();
 const PORT = process.env.PORT;
 const __dirname = path.resolve();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
 app.use(
   cors({
